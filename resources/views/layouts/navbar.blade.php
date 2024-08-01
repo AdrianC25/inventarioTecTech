6c082009e86fdc86cee353cae3278fd17dc07183
Adrian <nickcevallos50@gmail.com>
2024-07-31 21:08:47 -0500

    first commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,79 @@
+<!-- Navbar Superior -->
+<nav class="sb-topnav navbar navbar-expand-lg navbar-dark bg-dark">
+    <!-- Navbar Brand-->
+    {{-- <a class="navbar-brand ps-3" href="/"><img src="img/logoTecTech.jpg" alt="" class="rounded-circle"></a> --}}
+    <a class="navbar-brand ps-3" href="/">Inventory Tech</a>
+    <!-- Botón de menú colapsable -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu"
+        aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Navbar-->
+    <ul class="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#!">Settings</a></li>
+                <li>
+                    <hr class="dropdown-divider" />
+                </li>
+                <li><a class="dropdown-item" href="#!">Logout</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+        <!-- Sidebar del Dashboard -->
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
+            <div class="position-sticky">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/">
+                            <i class="fas fa-home"></i>
+                            Inicio
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/novedades">
+                            <i class="fas fa-tachometer-alt"></i>
+                            Administrar Ofertas
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                            aria-expanded="false">
+                            <i class="fas fa-columns"></i>
+                            Pedidos y Reservas
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Pedidos</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Reservas</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-file-alt"></i>
+                            Pages
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-chart-bar"></i>
+                            Charts
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-table"></i>
+                            Tables
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            @include('layouts.navbar')
+            {{-- @include('layouts.welcomeViews.home') --}}
+        </div>
+    </div>
+@endsection
+
+@section('css')
+    @parent
+    <style>
+        body {
+            font-size: 0.9rem;
+        }
+
+        .navbar-brand {
+            font-weight: bold;
+        }
+
+        .sidebar {
+            height: calc(100vh - 56px);
+            padding: 0;
+            position: fixed;
+            top: 56px;
+            /* Ajusta la posición para que quede debajo del navbar */
+            left: 0;
+            overflow-y: auto;
+            z-index: 100;
+            background-color: #343a40;
+        }
+
+        .sidebar .nav-link {
+            color: #ffffff;
+        }
+
+        .sidebar .nav-link:hover {
+            background-color: #495057;
+        }
+
+        .content {
+            margin-left: 250px;
+            padding: 15px;
+            margin-top: 56px;
+            /* Ajusta el margen superior para evitar que quede debajo del navbar */
+        }
+
+        @media (max-width: 767.98px) {
+            .sidebar {
+                position: relative;
+                height: auto;
+                top: 0;
+            }
+
+            .content {
+                margin-left: 0;
+            }
+        }
+    </style>
+@endsection
+
